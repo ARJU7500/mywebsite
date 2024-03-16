fc949c7fd8f93afa8ca413a32cce6c028edb304e
Arjun Negi <an096237@gmail.com>
2024-03-16 16:30:21 +0530

    completed

--- src/components/aboutUS/About.jsx
+++ src/components/aboutUS/About.jsx
@@ -29,7 +29,7 @@ function About() {
             have learned with friends.
           </p>
           <Link
-            to="../../../public/resume/asncollege.pdf"
+            to="./resume/asncollege.pdf"
             target="_blank"
             className="text-orange-600 border-solid border-2 border-orange-600 hover:bg-orange-600 hover:text-white focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none mt-40"
           >
