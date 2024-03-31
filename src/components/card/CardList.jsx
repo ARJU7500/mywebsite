53a22d6beb3246f9231a2466197665686b9b7caa
Arjun Negi <an096237@gmail.com>
2024-03-31 19:58:14 +0530

    last

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function CardList({ id, name, icon }) {
   return (
     <>
-      <div className="flex gap-2 justify-items-center shadow-md bg-slate-50 rounded-md w-24 p-1">
+      <div className="flex gap-2 justify-items-center shadow-md bg-slate-50 rounded-md w-26 p-1">
         <img src={icon} />
         <p className="text-orange-600">{name}</p>
       </div>
