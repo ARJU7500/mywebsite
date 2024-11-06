72151b07d6b6bc334c8f1a3e18c97e4ce833808d
Arju7500 <an096237@gmail.com>
2024-11-06 16:43:44 +0530

    update job

--- src/components/internship/Internship.jsx
+++ src/components/internship/Internship.jsx
@@ -5,7 +5,7 @@ function Internship() {
   return (
     <section id="internship">
       <h1 className="text-center mt-10 text-orange-600 text-5xl font-bold lg:px-10">
-        Internship
+        Internship/ WORK EXPERIENCE
       </h1>
       <p className=" font-semibold text-center mt-2 text-orange-600 text-lg ">
         My work experience as a front-end developer.
